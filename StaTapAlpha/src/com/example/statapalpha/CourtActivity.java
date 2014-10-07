61dd8adf3e29ef535fbd26f712e2858884d8bc6b
027lmp02 <loganprough@me.com>
2014-10-07 11:12:40 -0500

    Initial Upload
    Uploaded the StaTap Alpha files to open in Eclipse

---
+++ StaTapAlpha/src/com/example/statapalpha/CourtActivity.java
@@ -0,0 +1,13 @@
+package com.example.statapalpha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CourtActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_court);
+	}
+}
