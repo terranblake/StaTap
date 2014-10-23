796cf9a095741232bb9df3205cf419ff07855e63
Damian Vu <damianvu@outlook.com>
2014-10-23 12:51:05 -0500

    WOOOOOHOOOOO
    Yay

---
+++ StaTapAlpha/src/com/example/statapalpha/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.statapalpha;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// Array of options --> ArrayAdapter --> ListView
+
+// ListView: {Views: listviews.xml}
+
+
+public class MainActivity extends Activity {
+
+	
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        populateListView();
+        registerClickCallback();
+        
+    }
+    private void registerClickCallback() {
+		// TODO Auto-generated method stub
+    	ListView list = (ListView) findViewById(R.id.listViewMain);
+    	list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View viewClicked,
+					int position, long id) {
+					TextView textView = (TextView) viewClicked;
+					String message = "You Clicked # " + (position + 1) + ", which is string: " + textView.getText().toString();
+					Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+    public void newGame(View view) {
+    	Intent intent = new Intent(this, NewGame.class);
+    	startActivity(intent);
+    }
+	private void populateListView() {
+    	//Create list of items
+    	String[] gameNames = {"Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7", "Game 8", "Game 9", 
+    			"Game 10", "Game 11", "Game 12", "Game 13", "Game 14", "Game 15", "Game 16", "Game 17", "Game 18", "Game 19", "Game 20"};
+    	
+    	//Build Adapter
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+    			this,					// Context
+    			R.layout.listviews,		// Layout to use
+    			gameNames);				// Items to be displayed
+    			
+    	//Configure the List View
+    	ListView list = (ListView) findViewById(R.id.listViewMain);
+    	list.setAdapter(adapter);
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
