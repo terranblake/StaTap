d94797a840c7d6d6b531ad937d19a304f66903d5
027lmp02 <loganprough@me.com>
2014-10-23 11:41:02 -0500

    Started with functionality
    Created a player class to store player info

--- StaTapAlpha/src/com/example/statapalpha/CourtActivity.java
+++ StaTapAlpha/src/com/example/statapalpha/CourtActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class CourtActivity extends Activity {
 
+	public class player {
+		public int number = 0;
+		public int points = 0;
+		public int fouls = 0;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
