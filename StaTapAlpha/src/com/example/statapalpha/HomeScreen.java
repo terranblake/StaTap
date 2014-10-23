58dcd80c8e889b400f0beec0acfb684d54bdb4e3
Damian Vu <damianvu@outlook.com>
2014-10-23 13:05:47 -0500

    VUHOOOOOOOOOOO
    Awwww yisss

--- StaTapAlpha/src/com/example/statapalpha/HomeScreen.java
+++ StaTapAlpha/src/com/example/statapalpha/HomeScreen.java
@@ -1,9 +1,16 @@
 package com.example.statapalpha;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeScreen extends Activity {
 
@@ -11,8 +18,45 @@ public class HomeScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+        populateListView();
+        registerClickCallback();
 	}
+    public void newGame(View view) {
+    	Intent intent = new Intent(this, NewGameActivity.class);
+    	startActivity(intent);
+    }
+    private void registerClickCallback() {
+		// TODO Auto-generated method stub
+    	ListView list = (ListView) findViewById(R.id.listViewMain);
+    	list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> parent, View viewClicked,
+					int position, long id) {
+					TextView textView = (TextView) viewClicked;
+					String message = "You Clicked # " + (position + 1) + ", which is string: " + textView.getText().toString();
+					Toast.makeText(HomeScreen.this, message, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+    
+	private void populateListView() {
+    	//Create list of items
+    	String[] gameNames = {"Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7", "Game 8", "Game 9", 
+    			"Game 10", "Game 11", "Game 12", "Game 13", "Game 14", "Game 15", "Game 16", "Game 17", "Game 18", "Game 19", "Game 20"};
+    	
+    	//Build Adapter
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+    			this,					// Context
+    			R.layout.listviews,		// Layout to use
+    			gameNames);				// Items to be displayed
+    			
+    	//Configure the List View
+    	ListView list = (ListView) findViewById(R.id.listViewMain);
+    	list.setAdapter(adapter);
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- StaTapAlpha/src/com/example/statapalpha/NewGameActivity.java
+++ StaTapAlpha/src/com/example/statapalpha/NewGameActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.statapalpha.R;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NewGameActivity extends Activity {
 
@@ -11,8 +15,27 @@ public class NewGameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_newgame);
+		
+		populateListView();
 	}
 
+	private void populateListView() {
+		// TODO Auto-generated method stub
+    	//Create list of items
+    	String[] team1Names = {"Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9", 
+    			"Team 10", "Team 11", "Team 12", "Team 13", "Team 14", "Team 15", "Team 16", "Team 17", "Team 18", "Team 19", "Team 20"};
+    	
+    	//Build Adapter
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+    			this,					// Context
+    			R.layout.teamlistviews,		// Layout to use
+    			team1Names);				// Items to be displayed
+    			
+    	//Configure the List View
+    	ListView list = (ListView) findViewById(R.id.listView1);
+    	list.setAdapter(adapter);		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
