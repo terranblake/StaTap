8623273b2176ae54eb54af3bfc27113e859760a3
027lmp02 <loganprough@me.com>
2014-10-27 11:07:02 -0500

    Removed unnecessary import
    Removed unneeded import from CourtActivity.java

--- StaTapAlpha/src/com/example/statapalpha/CourtActivity.java
+++ StaTapAlpha/src/com/example/statapalpha/CourtActivity.java
@@ -2,7 +2,6 @@ package com.example.statapalpha;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class CourtActivity extends Activity {
