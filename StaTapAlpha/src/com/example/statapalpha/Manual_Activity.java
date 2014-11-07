e73026776c49cd1d34a4707b102faf3f993ceb8f
Hunter Faggot Richardson <terranblake@gmail.com>
2014-11-07 11:42:11 -0600

    Fixed Conflicts

--- StaTapAlpha/src/com/example/statapalpha/Manual_Activity.java
+++ StaTapAlpha/src/com/example/statapalpha/Manual_Activity.java
@@ -8,10 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
 import android.webkit.WebView;
-=======
->>>>>>> origin/master
+
 
 public class Manual_Activity extends Activity {
 
@@ -21,15 +19,12 @@ public class Manual_Activity extends Activity {
 		WebView webview = new WebView(this);
 		setContentView(webview);
 	
-		
-<<<<<<< HEAD
-=======
+
 		if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                 .add(R.id.container, new PlaceholderFragment()).commit();
 		}
 
->>>>>>> origin/master
 	}
 
 	@Override
