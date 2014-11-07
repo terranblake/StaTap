e97d276c045d3d3022211df77702ab1111e4b387
027lmp02 <loganprough@me.com>
2014-11-07 11:21:25 -0600

    Cleaned up code
    removed some unused imports

--- StaTapAlpha/src/com/example/statapalpha/NewGameActivity.java
+++ StaTapAlpha/src/com/example/statapalpha/NewGameActivity.java
@@ -2,23 +2,18 @@ package com.example.statapalpha;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.database.sqlite.*;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-
-import com.example.statapalpha.R;
-
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.example.statapalpha.R;
 
 public class NewGameActivity extends Activity {
 
