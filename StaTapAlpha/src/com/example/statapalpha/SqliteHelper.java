fe46aa04c29cde3b6df072df1976cfb0683b9002
027lmp02 <loganprough@me.com>
2014-11-13 12:56:30 -0600

    Added court sql stuff

--- StaTapAlpha/src/com/example/statapalpha/SqliteHelper.java
+++ StaTapAlpha/src/com/example/statapalpha/SqliteHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
  
 public class SqliteHelper extends SQLiteOpenHelper {
  
@@ -22,30 +21,36 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
 		db.execSQL("CREATE TABLE IF NOT EXISTS teamlist(id INTEGER PRIMARY KEY AUTOINCREMENT,Team_Names TEXT UNIQUE);");
+
+		// Create player table
+		db.execSQL("CREATE TABLE IF NOT EXISTS player(team_nbr NUMBER(02) UNIQUE, jersey_nbr NUMBER(02) UNIQUE, last_name VARCHAR(03), first_name VARCHAR(03));");
+		
+		// Create stats table
+		db.execSQL("CREATE TABLE IF NOT EXISTS stats(stat_nbr NUMBER(03), jersey_nbr NUMBER(02), team_nbr NUMBER(02), game_nbr NUMBER(03), half_nbr NUMBER(01), action VARCHAR2(3), x_coord NUMBER(03), y_coord NUMBER(03);");
     }
- public Cursor getTeams() {
-	 SQLiteDatabase db = this.getWritableDatabase();
-	 String[] projection = { "Team_Names" };
-	 return db.query("teamlist",projection, null, null, null, null, null, null);
-	 
- }
- public void addTeam(String teamname){
+    public Cursor getTeams() {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	String[] projection = { "Team_Names" };
+    	return db.query("teamlist",projection, null, null, null, null, null, null);
+    	
+    }
+    public void addTeam(String teamname){
 
-	 // 1. get reference to writable DB
-	 SQLiteDatabase db = this.getWritableDatabase();
+    	// 1. get reference to writable DB
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	
+    	// 2. create ContentValues to add key "column"/value
+    	ContentValues values = new ContentValues();
+    	values.put(TEAM_NAMES, teamname.toString()); // get title 
 
-	 // 2. create ContentValues to add key "column"/value
-	 ContentValues values = new ContentValues();
-	 values.put(TEAM_NAMES, teamname.toString()); // get title 
+    	// 3. insert
+    	db.insert("teamlist", // table name
+    	null, //nullColumnHack
+    	values); // key/value -> keys = column names/ values = column values
 
-	 // 3. insert
-	 db.insert("teamlist", // table name
-     null, //nullColumnHack
-     values); // key/value -> keys = column names/ values = column values
-
-	 // 4. close
-	 db.close(); 
- }
+    	// 4. close
+    	db.close(); 
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older books table if existed
@@ -53,6 +58,33 @@ public class SqliteHelper extends SQLiteOpenHelper {
  
         // create fresh books table
         this.onCreate(db);
+    }
+    
+    // For getting player info
+    public Cursor getPlayerNumber() {
+    	
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	String[] projection = { "stat_nbr" };
+    	return db.query("player",projection, null, null, null, null, null, null);
+    }
+    
+    // For updating player stats
+    public void updatePlayer(CourtActivity.player player){
+
+    	// 1. get reference to writable DB
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	
+    	// 2. create ContentValues to add key "column"/value
+    	ContentValues values = new ContentValues();
+    	//values.put(TEAM_NAMES, teamname.toString()); // get title 
+
+    	// 3. insert
+    	db.insert("teamlist", // table name
+    	null, //nullColumnHack
+    	values); // key/value -> keys = column names/ values = column values
+
+    	// 4. close
+    	db.close(); 
     }
  
 }
