ae749ebe3176c8cf4d8ee39c14586dd7f10b6901
Damian Vu <damianvu@outlook.com>
2014-12-05 11:37:30 -0600

    Almost done with stats screen!
    http://www.pcsalt.com/android/listview-using-baseadapter-android/

---
+++ StaTapAlpha/src/com/example/statapalpha/StatListData.java
@@ -0,0 +1,86 @@
+package com.example.statapalpha;
+
+/*
+ * ListData class will hold data for displaying in ListView
+ * */
+public class StatListData {
+ 
+        int Jersey;
+        int Points;
+        int FT;
+        int FG;
+        int Assists;
+        int Rebounds;
+        int Steals;
+        int TO;
+        int Fouls;
+        
+        String title;
+        int imgResId;
+ 
+        public Integer getJersey() {
+                return Jersey;
+        }
+ 
+        public void setJersey(Integer jerseynum) {
+                Jersey = jerseynum;
+        }
+ 
+        public Integer getPoints() {
+            return Points;
+        }
+
+        public void setPoints(Integer points) {
+            Points = points;
+        }
+        public Integer getFT() {
+            return FT;
+        }
+
+        public void setFT(Integer fts) {
+            FT = fts;
+        }
+        public Integer getFG() {
+            return FG;
+        }
+
+        public void setFG(Integer fgs) {
+            FG = fgs;
+        }
+        public Integer getAssists() {
+            return Assists;
+        }
+
+        public void setAssists(Integer ass) {
+            Assists = ass;
+        }
+        public Integer getRebounds() {
+            return Rebounds;
+        }
+
+        public void setRebounds(Integer rb) {
+            Rebounds = rb;
+        }
+        public Integer getSteals() {
+            return Steals;
+        }
+
+        public void setSteals(Integer stl) {
+            Steals = stl;
+        }
+        public Integer getTO() {
+            return TO;
+        }
+
+        public void setTO(Integer turnovers) {
+            TO = turnovers;
+        }
+        public Integer getFouls() {
+            return Fouls;
+        }
+
+        public void setFouls(Integer foul) {
+            Fouls = foul;
+        }
+
+}
